275f67210d0e5fadb4f2fc1a36a28d7f371ec3f7
martijn brekelmans <tijntje_7@msn.com>
2017-10-26 17:23:44 +0200

    Add documentation to lilyparser

--- DPA_Musicsheets/LilyPond/LilyParser.cs
+++ DPA_Musicsheets/LilyPond/LilyParser.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace DPA_Musicsheets.Lilypond
 {
+    // a parser is meant to assign meaning to the tokens from a tokenizer
+    // if you want to add support for various lilypond features,
+    // look in the tokenizer, and add a method for any of the tokens defined
+    // in the the tokenizer
+    // ie, tokens have regex groups that you should be able to
+    // easily parse using any of the built-in C# parsing methods
+    // refer to the documentation /INSIDE/ the ./lilyTokenizer
     class LilyParser
     {
 
