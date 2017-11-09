ffb914ae3d127e7ea41014f914e1e55dd500f0b6
martijn brekelmans <tijntje_7@msn.com>
2017-11-09 14:27:14 +0100

    Move midiparser to own file

--- DPA_Musicsheets/Managers/FileHandler.cs
+++ DPA_Musicsheets/Managers/FileHandler.cs
@@ -1,6 +1,7 @@
 ﻿
 using DPA_Musicsheets.Builders;
 using DPA_Musicsheets.Models;
+using DPA_Musicsheets.Parsers;
 using PSAMControlLibrary;
 using PSAMWPFControlLibrary;
 using Sanford.Multimedia.Midi;
@@ -83,38 +84,6 @@ namespace DPA_Musicsheets.Managers
 
 
         // Midi refactoring
-        private class MidiParser {
-            public Tuple<int, int> TimeSignature(MetaMessage message)
-            {
-                byte[] timeSignatureBytes = message.GetBytes();
-
-                int _beatNote = timeSignatureBytes[0];
-                int _beatsPerBar = (int)(1 / Math.Pow(timeSignatureBytes[1], -2));
-
-                return Tuple.Create<int, int>(_beatNote, _beatsPerBar);
-            }
-
-            public int Tempo(MetaMessage message)
-            {
-                byte[] tempoBytes = message.GetBytes();
-                int tempo = (tempoBytes[0] & 0xff) << 16 | (tempoBytes[1] & 0xff) << 8 | (tempoBytes[2] & 0xff);
-
-                return tempo;
-            }
-
-            public int Loudness(ChannelMessage message)
-            {
-                // in midi, ChannelMessage.Data2 is defined as the 'loudness'
-                return message.Data2;
-            }
-
-            // decouple actual Midi interface from DPA_Midi interface, even though we use
-            // the exact same terminology and data underneath.
-            public int AbsoluteTicks(MidiEvent message)
-            {
-                return message.AbsoluteTicks;
-            }
-        }
 
         public void LoadMidi(Sequence sequence)
         {
---
+++ DPA_Musicsheets/Parsers/MidiParser.cs
@@ -0,0 +1,44 @@
+﻿using Sanford.Multimedia.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Parsers
+{
+    public class MidiParser
+    {
+        public Tuple<int, int> TimeSignature(MetaMessage message)
+        {
+            byte[] timeSignatureBytes = message.GetBytes();
+
+            int _beatNote = timeSignatureBytes[0];
+            int _beatsPerBar = (int)(1 / Math.Pow(timeSignatureBytes[1], -2));
+
+            return Tuple.Create<int, int>(_beatNote, _beatsPerBar);
+        }
+
+        public int Tempo(MetaMessage message)
+        {
+            byte[] tempoBytes = message.GetBytes();
+            int tempo = (tempoBytes[0] & 0xff) << 16 | (tempoBytes[1] & 0xff) << 8 | (tempoBytes[2] & 0xff);
+
+            return tempo;
+        }
+
+        public int Loudness(ChannelMessage message)
+        {
+            // in midi, ChannelMessage.Data2 is defined as the 'loudness'
+            //          ChannelMessage.Data1 is defined as the 'pitch'
+            return message.Data2;
+        }
+
+        // decouple actual Midi interface from DPA_Midi interface, even though we use
+        // the exact same terminology and data underneath.
+        public int AbsoluteTicks(MidiEvent message)
+        {
+            return message.AbsoluteTicks;
+        }
+    }
+}
