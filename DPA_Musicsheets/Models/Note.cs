d7af33439eb524de55277323ea98419f3e7b700d
martijn brekelmans <tijntje_7@msn.com>
2017-11-09 12:44:55 +0100

    Simplify Note model

--- DPA_Musicsheets/Models/Note.cs
+++ DPA_Musicsheets/Models/Note.cs
@@ -8,12 +8,7 @@ namespace DPA_Musicsheets.Models
 {
     class Note
     {
-        public enum SIGNATURE { none, sharp, flat };
-
         public int duration;
         public int pitch;
-        public bool isRest;
-        public bool isDotted;
-        public SIGNATURE signature;
     }
 }
