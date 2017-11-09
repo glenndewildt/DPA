b987c2f7523aafeb37c533301c60eb0b5ff4e71a
martijn brekelmans <tijntje_7@msn.com>
2017-11-09 14:01:34 +0100

    Moved MidiLilyBuilder to own file

---
+++ DPA_Musicsheets/Builders/MidiLilyBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Builders
+{
+    public class MidiLilyBuilder
+    {
+        private StringBuilder _lilyProduct;
+
+        public MidiLilyBuilder()
+        {
+            _lilyProduct = new StringBuilder();
+        }
+
+        public void AddDefaultConfiguration()
+        {
+            _lilyProduct.AppendLine("\\relative c' ");
+            this.OpenScope();
+            _lilyProduct.AppendLine("\\clef treble");
+        }
+
+        public void AddTime(int beatNote, int beatsPerBar)
+        {
+            _lilyProduct.AppendLine($"\\time {beatNote}/{beatsPerBar}");
+        }
+
+        public void AddTempo(int bpm)
+        {
+            _lilyProduct.AppendLine($"\\tempo 4={bpm}");
+        }
+
+        public void AddNoteLength(string noteLength)
+        {
+            _lilyProduct.Append(noteLength);
+        }
+
+        public void AddBar()
+        {
+            _lilyProduct.AppendLine("|");
+        }
+
+        public void AddNote(string note)
+        {
+            _lilyProduct.Append(note);
+        }
+
+        public void AddNoteSeparator()
+        {
+            _lilyProduct.Append(" ");
+        }
+
+        public void OpenScope()
+        {
+            _lilyProduct.Append("{");
+        }
+
+        public void CloseScope()
+        {
+            _lilyProduct.Append("}");
+        }
+
+        /// <summary>
+        /// Appends anything that does not fit in with the
+        /// rest of the methods, does not add new line
+        /// </summary>
+        /// <param name="wildcard">Piece of custom content that will be added to the lily source</param>
+        public void AddCustom(string wildcard)
+        {
+            _lilyProduct.Append("r");
+        }
+
+        public string Build()
+        {
+            return _lilyProduct.ToString();
+        }
+    }
+}
